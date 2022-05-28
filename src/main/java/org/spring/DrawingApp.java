b10f9cb9cc30aa9522d73062edbae14cc095e222
test <mohitbharti1998@gmail.com>
2022-05-29 01:00:14 +0530

    Update commit

--- src/main/java/org/spring/DrawingApp.java
+++ src/main/java/org/spring/DrawingApp.java
@@ -11,7 +11,6 @@ public class DrawingApp {
 //        Triangle triangle = new Triangle();
 //        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
 
-        System.out.println("mohit");
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Triangle triangle = (Triangle) context.getBean("triangle");
         System.out.println(triangle.toString());
