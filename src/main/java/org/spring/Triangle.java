5e1ddc77045234fb52bdd7a381b63796bc0d7713
test <mohitbharti1998@gmail.com>
2022-05-29 00:42:12 +0530

    Initial commit

---
+++ src/main/java/org/spring/Triangle.java
@@ -0,0 +1,61 @@
+package org.spring;
+
+import java.util.List;
+
+public class Triangle {
+
+//    private Point pointA;
+//    private Point pointB;
+//    private Point pointC;
+//
+//    public Point getPointA() {
+//        return pointA;
+//    }
+//
+//    public void setPointA(Point pointA) {
+//        this.pointA = pointA;
+//    }
+//
+//    public Point getPointB() {
+//        return pointB;
+//    }
+//
+//    public void setPointB(Point pointB) {
+//        this.pointB = pointB;
+//    }
+//
+//    public Point getPointC() {
+//        return pointC;
+//    }
+//
+//    public void setPointC(Point pointC) {
+//        this.pointC = pointC;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Triangle{" +
+//                "pointA=" + pointA +
+//                ", pointB=" + pointB +
+//                ", pointC=" + pointC +
+//                '}';
+//    }
+
+
+    List<Point> points;
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "points=" + points +
+                '}';
+    }
+}
